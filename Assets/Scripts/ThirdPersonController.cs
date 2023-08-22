e929d1c1cd647dbe2bdeed8d4a1f27ce5bceac1c
Unarimit <1798907875@qq.com>
2023-08-22 22:25:26 +0800

    [bugfix] fix jump logic fail in aiming status

--- Assets/Scripts/ThirdPersonController.cs
+++ Assets/Scripts/ThirdPersonController.cs
@@ -330,7 +330,7 @@ namespace StarterAssets
         }
         private void JumpAndGravity()
         {
-            if (Grounded && !_input.aim)
+            if (Grounded)
             {
                 // reset the fall timeout timer
                 _fallTimeoutDelta = FallTimeout;
@@ -349,7 +349,7 @@ namespace StarterAssets
                 }
 
                 // Jump
-                if (_input.jump && _jumpTimeoutDelta <= 0.0f)
+                if (_input.jump && !_input.aim && _jumpTimeoutDelta <= 0.0f)
                 {
                     // the square root of H * -2 * G = how much velocity needed to reach desired height
                     _verticalVelocity = Mathf.Sqrt(JumpHeight * -2f * Gravity);
