e28328c806bfe416f8b11b15b198345996044011
Unarimit <1798907875@qq.com>
2023-08-24 16:13:43 +0800

    optimize enviorment
    1. 导入更多美术资源
    2. 凹一个比较有故事的场景
    通过灯光、火焰、烟雾效果

---
+++ Assets/Scripts/LightFleshContoller.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class LightFleshContoller : MonoBehaviour
+    {
+        [Tooltip("ms")]
+        public int Interval = 500;
+
+        [Tooltip("ms")]
+        public int Start = 0;
+
+        private int shake;
+        private MeshRenderer BoxColliderClick;
+        private void Awake()
+        {
+            shake += Start;
+            BoxColliderClick = gameObject.GetComponent<MeshRenderer>();
+        }
+
+        private void Update()
+        {
+            shake += (int)(Time.deltaTime*1000);
+            if (shake % Interval > Interval / 2)
+            {
+                BoxColliderClick.enabled = true;
+            }
+            else
+            {
+                BoxColliderClick.enabled = false;
+            }
+        }
+    }
+}
