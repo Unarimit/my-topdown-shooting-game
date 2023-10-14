6a2502c9c21603d790e9a45301731b9dc181c5c5
Unarimit <1798907875@qq.com>
2023-10-14 13:47:26 +0800

    [game logic] add key hits logic

---
+++ Assets/Scripts/CombatLogic/UILogic/KeyTipsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Assets.Scripts.CombatLogic.UILogic
+{
+    public class KeyTipsController : MonoBehaviour
+    {
+
+        public TextMeshProUGUI keyPromptText;
+        public InputActionAsset inputActionAsset;
+
+        private void Start()
+        {
+            GenerateKeyPrompts();
+        }
+
+        private void GenerateKeyPrompts()
+        {
+            var inputActions = inputActionAsset.actionMaps;
+            var sb = new StringBuilder();
+            foreach (var actionMap in inputActions)
+            {
+                foreach (var action in actionMap.actions)
+                {
+                    // 忽略不是按键类型的操作
+                    if (!action.bindings[0].isPartOfComposite)
+                    {
+                        if (action.bindings[0].effectivePath.Contains("/"))  sb.Append($"{action.name}: " +
+                            $"{action.bindings[0].effectivePath.Split("/")[1]} \n");
+                        else sb.Append($"{action.name}: {action.bindings[0].effectivePath} \n");
+
+                    }
+                }
+            }
+            keyPromptText.text = sb.ToString();
+        }
+    }
+}
