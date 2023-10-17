a1bce6a087291ac15230aaa3415d8d13039f444f
Unarimit <1798907875@qq.com>
2023-10-17 16:56:02 +0800

    [Game logic]  add partial grenade logic

---
+++ Assets/Scripts/CombatLogic/WeaponLogic/Grenade/GrenadeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.CombatLogic
+{
+    /// <summary>
+    /// 控制爆炸效果的演出
+    /// </summary>
+    public class GrenadeController : MonoBehaviour, ICombatDelayWeapon
+    {
+        public void DoDelayAction()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Scripts/CombatLogic/WeaponLogic/Interface/ICombatDelayWeapon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.CombatLogic
+{
+    public interface ICombatDelayWeapon
+    {
+        /// <summary>
+        /// 执行被延迟的动作
+        /// </summary>
+        public void DoDelayAction();
+    }
+}
