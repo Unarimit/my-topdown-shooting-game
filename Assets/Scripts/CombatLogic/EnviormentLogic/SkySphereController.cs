57a0dbd2632035c95fc3a00792b48d2f74d92c1b
Unarimit <1798907875@qq.com>
2023-12-07 11:17:14 +0800

    add sky sphere

---
+++ Assets/Scripts/CombatLogic/EnviormentLogic/SkySphereController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.CombatLogic.EnviormentLogic
+{
+    /// <summary>
+    /// 控制天空球自转
+    /// </summary>
+    internal class SkySphereController : MonoBehaviour
+    {
+        private void Update()
+        {
+            transform.Rotate(new Vector3(0, 1 * Time.deltaTime, 0));
+        }
+    }
+}
