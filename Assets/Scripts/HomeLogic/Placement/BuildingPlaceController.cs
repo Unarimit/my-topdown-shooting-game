2ffb9ca288ac2a668361db450c1debfe2e1d30ef
Unarimit <1798907875@qq.com>
2023-12-12 18:47:21 +0800

    [Game logic] add place building grid method
    copy from unity TD template

---
+++ Assets/Scripts/HomeLogic/Placement/BuildingPlaceController.cs
@@ -0,0 +1,110 @@
+﻿
+using Assets.Scripts.Entities;
+using UnityEngine;
+
+namespace Assets.Scripts.HomeLogic.Placement
+{
+    internal class BuildingPlaceController : MonoBehaviour
+    {
+        /// <summary>
+		/// True if we're at a valid world position
+		/// </summary>
+		protected bool m_ValidPos;
+        /// <summary>
+		/// Target world position
+		/// </summary>
+		protected Vector3 m_TargetPosition;
+        /// <summary>
+		/// The list of attached mesh renderers 
+		/// </summary>
+		protected MeshRenderer[] m_MeshRenderers;
+
+        /// <summary>
+		/// The two materials used to represent valid and invalid placement, respectively
+		/// </summary>
+		public Material material;
+
+        public Material invalidPositionMaterial;
+        /// <summary>
+		/// Movement velocity for smooth damping
+		/// </summary>
+		protected Vector3 m_MoveVel;
+        /// <summary>
+        /// Movement damping factor
+        /// </summary>
+        public float dampSpeed = 0.075f;
+
+        public Building Building { get; private set; } = new Building() { Dimensions = new Vector2Int(1, 1) };
+
+        private void Awake()
+        {
+            m_MeshRenderers = GetComponentsInChildren<MeshRenderer>();
+        }
+
+        /// <summary>
+        /// Moves this ghost to a given world position
+        /// </summary>
+        /// <param name="worldPosition">The new position to move to in world space</param>
+        /// <param name="rotation">The new rotation to adopt, in world space</param>
+        /// <param name="validLocation">Whether or not this position is valid. Ghost may display differently
+        /// over invalid locations</param>
+        public virtual void Move(Vector3 worldPosition, Quaternion rotation, bool validLocation)
+        {
+            m_TargetPosition = worldPosition;
+
+            if (!m_ValidPos)
+            {
+                // Immediately move to the given position
+                m_ValidPos = true;
+                transform.position = m_TargetPosition;
+            }
+
+            transform.rotation = rotation;
+            foreach (MeshRenderer meshRenderer in m_MeshRenderers)
+            {
+                meshRenderer.sharedMaterial = validLocation ? material : invalidPositionMaterial;
+            }
+        }
+        /// <summary>
+        /// Show this ghost
+        /// </summary>
+        public virtual void Show()
+        {
+            if (!gameObject.activeSelf)
+            {
+                gameObject.SetActive(true);
+                m_MoveVel = Vector3.zero;
+
+                m_ValidPos = false;
+            }
+        }
+
+        /// <summary>
+		/// Hide this ghost
+		/// </summary>
+		public virtual void Hide()
+        {
+            gameObject.SetActive(false);
+        }
+
+        /// <summary>
+		/// Damp the movement of the ghost
+		/// </summary>
+		protected virtual void Update()
+        {
+            Vector3 currentPos = transform.position;
+
+            if (Vector3.SqrMagnitude(currentPos - m_TargetPosition) > 0.01f)
+            {
+                currentPos = Vector3.SmoothDamp(currentPos, m_TargetPosition, ref m_MoveVel, dampSpeed);
+
+                transform.position = currentPos;
+            }
+            else
+            {
+                m_MoveVel = Vector3.zero;
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/HomeLogic/Placement/PlacementController.cs
@@ -0,0 +1,120 @@
+﻿using Assets.Scripts.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TowerDefense.Towers.Placement;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Assets.Scripts.HomeLogic.Placement
+{
+    internal class PlacementController : MonoBehaviour
+    {   
+        /// <summary>
+        /// Placement area ghost tower is currently on
+        /// </summary>
+        IPlacementArea m_CurrentArea;
+        /// <summary>
+		/// Grid position ghost tower in on
+		/// </summary>
+		Vector2Int m_GridPosition;
+        /// <summary>
+		/// Current tower placeholder. Will be null if not in the <see cref="State.Building" /> state.
+		/// </summary>
+		public BuildingPlaceController m_CurrentBuilding;
+        /// <summary>
+		/// Tracks if the ghost is in a valid location and the player can afford it
+		/// </summary>
+		bool m_GhostPlacementPossible;
+
+        private void Update()
+        {
+            MoveGhost(false);
+        }
+
+        /// <summary>
+		/// Move the ghost to the pointer's position
+		/// </summary>
+		/// <param name="pointer">The pointer to place the ghost at</param>
+		/// <param name="hideWhenInvalid">Optional parameter for whether the ghost should be hidden or not</param>
+		/// <exception cref="InvalidOperationException">If we're not in the correct state</exception>
+		protected void MoveGhost(bool hideWhenInvalid = true)
+        {
+            if (m_CurrentBuilding == null) //  || !isBuilding
+            {
+                throw new InvalidOperationException(
+                    "Trying to position a tower ghost while the UI is not currently in the building state.");
+            }
+
+            var ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+            var hits = Physics.RaycastAll(ray, 100f, LayerMask.GetMask(new string[] { "BuildingGrid" }));
+
+            if (hits.Length == 1)
+            {
+                MoveGhostWithRaycastHit(hits[0]);
+            }
+            else
+            {
+                MoveGhostOntoWorld(ray, hideWhenInvalid);
+            }
+        }
+
+
+        private void MoveGhostWithRaycastHit(RaycastHit raycast)
+        {
+            // We successfully hit one of our placement areas
+            // Try and get a placement area on the object we hit
+            m_CurrentArea = raycast.collider.GetComponent<IPlacementArea>();
+
+            if (m_CurrentArea == null)
+            {
+                Debug.LogError("There is not an IPlacementArea attached to the collider found on the m_PlacementAreaMask");
+                return;
+            }
+            m_GridPosition = m_CurrentArea.WorldToGrid(raycast.point, m_CurrentBuilding.Building.Dimensions);
+            TowerFitStatus fits = m_CurrentArea.Fits(m_GridPosition, m_CurrentBuilding.Building.Dimensions);
+
+            m_CurrentBuilding.Show();
+            m_GhostPlacementPossible = fits == TowerFitStatus.Fits;
+            m_CurrentBuilding.Move(m_CurrentArea.GridToWorld(m_GridPosition, m_CurrentBuilding.Building.Dimensions),
+                                m_CurrentArea.transform.rotation,
+                                m_GhostPlacementPossible);
+        }
+        /// <summary>
+		/// The radius of the sphere cast 
+		/// for checking ghost placement
+		/// </summary>
+		public float sphereCastRadius = 1;
+        /// <summary>
+		/// The physics layer for moving the ghost around the world
+		/// when the placement is not valid
+		/// </summary>
+		public LayerMask ghostWorldPlacementMask;
+        /// <summary>
+		/// Move ghost with the given ray
+		/// </summary>
+		protected virtual void MoveGhostOntoWorld(Ray ray, bool hideWhenInvalid)
+        {
+            m_CurrentArea = null;
+
+            if (!hideWhenInvalid)
+            {
+                RaycastHit hit;
+                // check against all layers that the ghost can be on
+                Physics.SphereCast(ray, sphereCastRadius, out hit, 1000f, ghostWorldPlacementMask);
+                if (hit.collider == null)
+                {
+                    return;
+                }
+                m_CurrentBuilding.Show();
+                m_CurrentBuilding.Move(hit.point, hit.collider.transform.rotation, false);
+            }
+            else
+            {
+                m_CurrentBuilding.Hide();
+            }
+        }
+    }
+}
