4544103a65971e1db127e1e19723b5d22f3515fb
Unarimit <1798907875@qq.com>
2024-01-25 10:13:46 +0800

    try fix 'Wind Back' AI Behavior

--- Assets/Scripts/CombatLogic/Characters/BehaviorTreeExtend/WindBackTask.cs
+++ Assets/Scripts/CombatLogic/Characters/BehaviorTreeExtend/WindBackTask.cs
@@ -28,23 +28,29 @@ namespace Assets.Scripts.CombatLogic.Characters.BehaviorTreeExtend
             Vector3 pivot = TargetGroup.Value[0].transform.position; // 旋转中心点坐标
             Vector3 point = transform.position; // 要旋转的点坐标
 
-            // 计算旋转后的位置
-            Vector3 rotatedPoint1 = Quaternion.Euler(0f, 90f, 0f) * (point - pivot).normalized * 10 + pivot;
-            Vector3 rotatedPoint2 = Quaternion.Euler(0f, -90f, 0f) * (point - pivot).normalized * 10 + pivot;
-
+            float myInterpolat(float begin, float end, float x)
+            {
+                // return begin + (2.2f * x -  1.2f * x * x) * (end - begin);
+                return begin + x * x * (end - begin);
+            }
+            
             void generateDests(float angle, float curDistance)
             {
                 dests = new Queue<Vector3>();
                 for(int i = 0; i < 10; i++)
                 {
-                    dests.Enqueue(Quaternion.Euler(0f, angle / 10 * i, 0f) * (point - pivot).normalized * Mathf.SmoothStep(curDistance, 10f, i/9) + pivot);
+                    dests.Enqueue(Quaternion.Euler(0f, angle / 10 * i, 0f) * (point - pivot).normalized * myInterpolat(curDistance, 10f, (float)i/9) + pivot);
                 }
             }
 
+            // 计算旋转后的位置
+            Vector3 rotatedPoint1 = Quaternion.Euler(0f, 90f, 0f) * (point - pivot).normalized * 10 + pivot;
+            Vector3 rotatedPoint2 = Quaternion.Euler(0f, -90f, 0f) * (point - pivot).normalized * 10 + pivot;
+
             // 选择距离短的
-            if(Vector3.Distance(rotatedPoint1, point) < Vector3.Distance(rotatedPoint2, point)) generateDests(90f, Vector3.Distance(point, pivot));
+            if (Vector3.Distance(rotatedPoint1, point) < Vector3.Distance(rotatedPoint2, point)) generateDests(90f, Vector3.Distance(point, pivot));
             else generateDests(-90f, Vector3.Distance(point, pivot));
-            agent.SetDestination(dests.Peek());
+            SetLegalDestination();
             agent.speed *= speedFactor;
         }
 
@@ -52,7 +58,7 @@ namespace Assets.Scripts.CombatLogic.Characters.BehaviorTreeExtend
         {
             // todo：被攻击时返回sucess终止行为
 
-            if (dests.Count != 0 && Vector3.Distance(transform.position, dests.Peek()) < 1f) {
+            if (dests.Count != 0 && (Vector3.Distance(transform.position, dests.Peek()) < 1f || agent.isPathStale)) {
                 dests.Dequeue();
                 if (dests.Count == 0)
                 {
@@ -61,7 +67,7 @@ namespace Assets.Scripts.CombatLogic.Characters.BehaviorTreeExtend
                 }
                 else
                 {
-                    agent.SetDestination(dests.Peek());
+                    SetLegalDestination();
                     return TaskStatus.Running;
                 }
             } 
@@ -71,6 +77,16 @@ namespace Assets.Scripts.CombatLogic.Characters.BehaviorTreeExtend
         public override void OnEnd()
         {
             agent.speed /= speedFactor;
+        }
+        /// <summary>
+        /// 跳过不可达点
+        /// </summary>
+        private void SetLegalDestination()
+        {
+            while(dests.Count > 0 && agent.SetDestination(dests.Peek()) is false)
+            {
+                dests.Dequeue();
+            }
         }
     }
 }
