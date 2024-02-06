ed59ef822e1c65a2fb96e5e774c52c7740457021
Unarimit <1798907875@qq.com>
2024-02-06 16:47:13 +0800

    update dll, gen xLua config code and test

---
+++ Assets/Scripts/XLuaConfig.cs
@@ -0,0 +1,19 @@
+﻿using Assets.Scripts.Services;
+using Assets.Scripts.Services.Database;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+namespace Assets.Scripts
+{
+    public static class XLuaConfig
+    {
+        [CSharpCallLua]
+        public static List<Type> mymodule_lua_call_cs_list = new List<Type>()
+        {
+            typeof(Func<IGameDatabase, bool>)
+        };
+
+    }
+}
---
+++ Assets/XLua/Gen/DelegatesGensBridge.cs
@@ -0,0 +1,59 @@
+﻿#if USE_UNI_LUA
+using LuaAPI = UniLua.Lua;
+using RealStatePtr = UniLua.ILuaState;
+using LuaCSFunction = UniLua.CSharpFunctionDelegate;
+#else
+using LuaAPI = XLua.LuaDLL.Lua;
+using RealStatePtr = System.IntPtr;
+using LuaCSFunction = XLua.LuaDLL.lua_CSFunction;
+#endif
+
+using System;
+
+
+namespace XLua
+{
+    public partial class DelegateBridge : DelegateBridgeBase
+    {
+		
+		public bool __Gen_Delegate_Imp0(Assets.Scripts.Services.Database.IGameDatabase p0)
+		{
+#if THREAD_SAFE || HOTFIX_ENABLE
+            lock (luaEnv.luaEnvLock)
+            {
+#endif
+                RealStatePtr L = luaEnv.rawL;
+                int errFunc = LuaAPI.pcall_prepare(L, errorFuncRef, luaReference);
+                ObjectTranslator translator = luaEnv.translator;
+                translator.PushAny(L, p0);
+                
+                PCall(L, 1, 1, errFunc);
+                
+                
+                bool __gen_ret = LuaAPI.lua_toboolean(L, errFunc + 1);
+                LuaAPI.lua_settop(L, errFunc - 1);
+                return  __gen_ret;
+#if THREAD_SAFE || HOTFIX_ENABLE
+            }
+#endif
+		}
+        
+        
+		static DelegateBridge()
+		{
+		    Gen_Flag = true;
+		}
+		
+		public override Delegate GetDelegateByType(Type type)
+		{
+		
+		    if (type == typeof(System.Func<Assets.Scripts.Services.Database.IGameDatabase, bool>))
+			{
+			    return new System.Func<Assets.Scripts.Services.Database.IGameDatabase, bool>(__Gen_Delegate_Imp0);
+			}
+		
+		    return null;
+		}
+	}
+    
+}
