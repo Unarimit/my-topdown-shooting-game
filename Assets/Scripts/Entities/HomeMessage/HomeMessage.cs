89aed6aed75a99b2f7dc514eed4dcc5e44db1fe4
Unarimit <1798907875@qq.com>
2024-02-23 22:34:47 +0800

    fix serialize control bug

--- Assets/Scripts/Entities/HomeMessage/HomeMessage.cs
+++ Assets/Scripts/Entities/HomeMessage/HomeMessage.cs
@@ -7,12 +7,14 @@ namespace Assets.Scripts.Entities.HomeMessage
     public class HomeMessage
     {
         private static int _ID = 0;
-        [NonSerialized]
         /// <summary> 临时Id，不应序列化存储 </summary>
+        [NonSerialized]
         public int Id;
         public int Day;
         /// <summary> 用于序列化 </summary>
         public string MessageActionId;
+
+        [field: NonSerialized]
         internal Action<HomeContextManager> MessageAction { get; set; }
         public HomeMessage()
         {
