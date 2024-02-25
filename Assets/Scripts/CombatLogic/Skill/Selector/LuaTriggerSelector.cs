79d240f3d031e0892dc5aec22f126cd2b1f944f2
Unarimit <1798907875@qq.com>
2024-02-25 18:20:02 +0800

    [Game logic] add ObjectPool in skill system

--- Assets/Scripts/CombatLogic/Skill/Selector/LuaTriggerSelector.cs
+++ Assets/Scripts/CombatLogic/Skill/Selector/LuaTriggerSelector.cs
@@ -3,11 +3,7 @@ using Assets.Scripts.CombatLogic.Skill.Releaser;
 using Assets.Scripts.Entities;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
-using XLua;
 
 namespace Assets.Scripts.CombatLogic.Skill.Selector
 {
@@ -21,14 +17,16 @@ namespace Assets.Scripts.CombatLogic.Skill.Selector
         Action<GameObject, Transform, Vector3> action;
         public void Init(List<IImpactor> impectors, BaseReleaser releaser)
         {
+            // reset碰撞系统
+            GetComponent<Collider>().enabled = true;
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+
             _impactors = impectors;
             _skill = releaser.Skill;
             action =  MyServices.LuaEnv.Global.GetInPath<Action<GameObject, Transform, Vector3>>(_skill.SkillSelector.Data);
             action(gameObject, releaser.Caster, releaser.Aim);
 
-            // reset碰撞系统
-            GetComponent<Collider>().enabled = true;
-            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
         }
         private void OnTriggerEnter(Collider collision)
         {
